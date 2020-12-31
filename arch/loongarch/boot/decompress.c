4ac5cf100a74fbd0c8915c3043661b53d0f13fde
Huacai Chen <chenhuacai@loongson.cn>
2020-12-31 15:13:33 +0800

    LoongArch: Add kenel hacking options support
    Add some kenel hacking options support for LoongArch, including sysrq
    and zboot debugging.
    
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/boot/decompress.c
+++ arch/loongarch/boot/decompress.c
@@ -21,8 +21,14 @@ unsigned long free_mem_end_ptr;
 /* The linker tells us where the image is. */
 extern unsigned char __image_begin, __image_end;
 
+/* debug interfaces  */
+#ifdef CONFIG_DEBUG_ZBOOT
+extern void puts(const char *s);
+extern void puthex(unsigned long long val);
+#else
 #define puts(s) do {} while (0)
 #define puthex(val) do {} while (0)
+#endif
 
 void error(char *x)
 {
