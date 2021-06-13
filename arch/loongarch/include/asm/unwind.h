92556469353d3d9cd2356237123717b607ee7fb0
Huacai Chen <chenhuacai@loongson.cn>
2021-06-13 18:13:09 +0800

    LoongArch: Add orc stack unwinder support
    Signed-off-by: Jinyang He <hejinyang@loongson.cn>
    Signed-off-by: Huacai Chen <chenhuacai@loongson.cn>

--- arch/loongarch/include/asm/unwind.h
+++ arch/loongarch/include/asm/unwind.h
@@ -8,20 +8,23 @@
 #define _ASM_UNWIND_H
 
 #include <linux/sched.h>
+#include <linux/module.h>
 
 #include <asm/stacktrace.h>
 
 enum unwinder_type {
 	UNWINDER_GUESS,
 	UNWINDER_PROLOGUE,
+	UNWINDER_ORC,
 };
 
 struct unwind_state {
 	char type; /* UNWINDER_XXX */
 	struct stack_info stack_info;
 	struct task_struct *task;
+	int graph_idx;
 	bool first, error;
-	unsigned long sp, pc, ra;
+	unsigned long sp, pc, fp, ra;
 };
 
 void unwind_start(struct unwind_state *state,
@@ -39,4 +42,13 @@ static inline bool unwind_error(struct unwind_state *state)
 	return state->error;
 }
 
+#ifdef CONFIG_UNWINDER_ORC
+void unwind_init(void);
+void unwind_module_init(struct module *mod, void *orc_ip, size_t orc_ip_size,
+			void *orc, size_t orc_size);
+#else
+static inline void unwind_init(void) {}
+static inline void unwind_module_init(struct module *mod, void *orc_ip,
+			size_t orc_ip_size, void *orc, size_t orc_size) {}
+#endif /* CONFIG_UNWINDER_ORC */
 #endif /* _ASM_UNWIND_H */
