783faf4b53c0bdcfc361eacc7eafa3843e41b712
Youling Tang <tangyouling@loongson.cn>
2021-11-01 17:46:27 +0800

    tools/objtool: Add basic support for LoongArch
    Change-Id: Iab28e11f9fb5a6ed1bc979dec0c1f9d2a00ddc0b
    Signed-off-by: Jinyang He <hejinyang@loongson.cn>
    Signed-off-by: Youling Tang <tangyouling@loongson.cn>

--- tools/objtool/include/objtool/check.h
+++ tools/objtool/include/objtool/check.h
@@ -40,6 +40,7 @@ struct instruction {
 	struct list_head list;
 	struct hlist_node hash;
 	struct list_head call_node;
+	struct list_head orbit_node;
 	struct section *sec;
 	unsigned long offset;
 	unsigned int len;
@@ -54,8 +55,9 @@ struct instruction {
 	   restore		: 1,
 	   retpoline_safe	: 1,
 	   noendbr		: 1,
-	   entry		: 1;
-		/* 7 bit hole */
+	   entry		: 1,
+	   not_sibling_call	: 1;
+		/* 6 bit hole */
 
 	s8 instr;
 	u8 visited;
@@ -94,8 +96,22 @@ static inline bool is_jump(struct instruction *insn)
 	return is_static_jump(insn) || is_dynamic_jump(insn);
 }
 
+bool is_sibling_call(struct instruction *insn);
+void save_reg(struct cfi_state *cfi, unsigned char reg, int base, int offset);
+void restore_reg(struct cfi_state *cfi, unsigned char reg);
+
 struct instruction *find_insn(struct objtool_file *file,
 			      struct section *sec, unsigned long offset);
+struct instruction *next_insn_same_func(struct objtool_file *file,
+					struct instruction *insn);
+struct instruction *prev_insn_same_sym(struct objtool_file *file,
+				       struct instruction *insn);
+int add_jump_table(struct objtool_file *file, struct instruction *insn,
+				struct reloc *table);
+bool arch_has_valid_stack_frame(struct insn_state *state);
+int arch_classify_symbols(struct objtool_file *file);
+int arch_create_static_call_sections(struct objtool_file *file);
+int arch_handle_insn_ops(struct instruction *insn, struct instruction *next_insn, struct insn_state *state);
 
 #define for_each_insn(file, insn)					\
 	list_for_each_entry(insn, &file->insn_list, list)
@@ -105,5 +121,34 @@ struct instruction *find_insn(struct objtool_file *file,
 	     insn && &insn->list != &file->insn_list &&			\
 			insn->sec == sec;				\
 	     insn = list_next_entry(insn, list))
+
+
+#define func_last_orbit(p)						\
+	(list_first_entry_or_null(p, struct instruction, orbit_node))
+
+#define func_for_each_insn(file, func, insn)				\
+	for (insn = find_insn(file, func->sec, func->offset);		\
+	     insn;							\
+	     insn = next_insn_same_func(file, insn))
+
+#define sym_for_each_insn(file, sym, insn)				\
+	for (insn = find_insn(file, sym->sec, sym->offset);		\
+	     insn && &insn->list != &file->insn_list &&			\
+		insn->sec == sym->sec &&				\
+		insn->offset < sym->offset + sym->len;			\
+	     insn = list_next_entry(insn, list))
+
+#define sym_for_each_insn_continue_reverse(file, sym, insn)		\
+	for (insn = list_prev_entry(insn, list);			\
+	     &insn->list != &file->insn_list &&				\
+		insn->sec == sym->sec && insn->offset >= sym->offset;	\
+	     insn = list_prev_entry(insn, list))
+
+#define sec_for_each_insn_from(file, insn)				\
+	for (; insn; insn = next_insn_same_sec(file, insn))
+
+#define sec_for_each_insn_continue(file, insn)				\
+	for (insn = next_insn_same_sec(file, insn); insn;		\
+	     insn = next_insn_same_sec(file, insn))
 
 #endif /* _CHECK_H */
