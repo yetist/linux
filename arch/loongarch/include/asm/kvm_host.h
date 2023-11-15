8eac4260c6f6bb44e0054b8848550082c2b3a0e1
Tianrui Zhao <zhaotianrui@loongson.cn>
2023-11-15 14:20:57 +0800

    LoongArch: KVM: Add lasx support
    This patch adds LASX support for LoongArch KVM. The LASX means
    LoongArch 256-bits vector instruction.
    There will be LASX exception in KVM when guest use the LASX
    instruction. KVM will enable LASX and restore the vector
    registers for guest then return to guest to continue running.
    
    Signed-off-by: Tianrui Zhao <zhaotianrui@loongson.cn>
    Change-Id: I8495cc5583892ea388e469a2e30e50dff932fc21

--- arch/loongarch/include/asm/kvm_host.h
+++ arch/loongarch/include/asm/kvm_host.h
@@ -95,6 +95,7 @@ enum emulation_result {
 #define KVM_LARCH_SWCSR_LATEST	(0x1 << 1)
 #define KVM_LARCH_HWCSR_USABLE	(0x1 << 2)
 #define KVM_LARCH_LSX		(0x1 << 3)
+#define KVM_LARCH_LASX		(0x1 << 4)
 
 struct kvm_vcpu_arch {
 	/*
@@ -179,6 +180,11 @@ static inline void writel_sw_gcsr(struct loongarch_csrs *csr, int reg, unsigned 
 static inline bool kvm_guest_has_lsx(struct kvm_vcpu_arch *arch)
 {
 	return arch->cpucfg[2] & CPUCFG2_LSX;
+}
+
+static inline bool kvm_guest_has_lasx(struct kvm_vcpu_arch *arch)
+{
+	return arch->cpucfg[2] & CPUCFG2_LASX;
 }
 
 /* Debug: dump vcpu state */
