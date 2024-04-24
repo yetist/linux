5a14cfe6017238fa9e8a3f0da4f46a06f1adb7a8
Xiaotian Wu <wuxiaotian@loongson.cn>
2024-04-24 17:47:47 +0800

    makedist.sh: 6.8.6

--- makedist.sh
+++ makedist.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 name=linux
-version=6.8.0
+version=6.8.6
 tarballs=$name-$version-`date +%Y%m%d`.tar.xz
 
 git archive --format=tar.xz --prefix=$name-$version/ -o $tarballs HEAD
